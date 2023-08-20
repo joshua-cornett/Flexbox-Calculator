e993a797571e8e2673c38a1f9da6e1a20a9621c4
Joshua Cornett <joshua.m.cornett@proton.me>
2023-08-20 03:36:55 -0700

    Can calculate, but has bugs
    Made the calculator functional, refactoring much of the code in the process. It has bugs that need working out, particularly when working with multiple operations without explicitly clicking "=".  Utilizes a recursive function for evaluation that is capable of following PEMDAS.
    
    P.S. - forgot to mention but it now has an expressions section, which now stores prior expressions. It occurred to me that this is essentially a history, but still, I thought it was a neat addition.

--- scripts/script.js
+++ scripts/script.js
@@ -1,140 +1,151 @@
 console.log("Hello");
 
-let buttons = document.querySelectorAll('.calc-button');
+const buttons = document.querySelectorAll('.calc-button');
 
 //grab the display (an element inside the display div holds the shown number)
-let display = document.getElementById("innerDisplay");
-let expressionDisplay = document.getElementById("expression");
+const calcDisplay = document.getElementById("displayValue");
 
-let operand = display.innerHTML = "0";
+//grab the expression display
+//holds expressions as p elements
+const exprDisplay = document.getElementById("expressionBox");
+
+let operand = calcDisplay.innerHTML = "0";
+
+let expressions = [];
 let expression = [];
 
-let state = "clear";
+const ops = ['(',')','e','×','÷','+','-'];
 
 buttons.forEach((button) => {
   const buttonVal = button.querySelector('p').innerText;
   button.addEventListener('click', () => {
     //call function to process
-    processButton(buttonVal);
+    const updates = processButton(buttonVal);
+    if(updates[0]) { //update calculator display
+      calcDisplay.innerHTML = operand;
+    }
+    if(updates[1]) { //update expression display
+      exprDisplay.lastElementChild.innerHTML = expression.join(' ');
+    }
+    if(updates[2]) { //add a new expression placeholder in the expression box
+      addNewExpression();
+    }
   });
 });
 
-//add the value to expression array
+const addNewExpression = () => {
+  expression = [];
+  const newPElement = exprDisplay.appendChild(document.createElement('p'));
+  newPElement.setAttribute('class','expressionValue');
+}
+
 const processButton = (bVal) => {
 
-  let updateExpression = false;
-
-  switch (bVal) {
-
-    case 'C':
-      //clear
-      C();
-      updateExpression = true;
-      break;
-
-    case 'DEL':
-      //delete
-      DEL();
-      break;
-
-    case '=':
-      //evaulate
-      expression.push(operand);
-      evaluate(expression);
-      updateExpression = true;
-      break;
-
-    default:
-      //must be a operand or operator
-      let isOperand = !isNaN(parseInt(bVal));
-      let isOperator = !isOperand;
-
-      switch(state) {
-
-        case "clear":
-          if (isOperand) {
-            //operand on clear
-            display.innerHTML = operand = bVal;
-            state = "operand"; 
-          }
-          else {
-            //operator on clear
-            expression.push(operand,bVal);
-            updateExpression = true;
-            state = "operator";
-          }
-          break;
-
-        case "operand":
-          if (isOperand) { //operand on operand
-            //add it so long as it doesn't start with zero
-            operand += bVal;
-            display.innerHTML += bVal;
-          }
-          else { //operator on operand
-            expression.push(operand,bVal);
-            updateExpression = true;
-            state = "operator";
-          }
-          break;
-
-        case "operator":
-          if(isOperand) { //operand on operator
-            //incorporate new operand and update display
-            display.innerHTML = operand = bVal;
-            state = "operand";
-          }
-          else { //operator on operator
-            //override previous operator
-            expression[expression.length -1] = bVal;
-            updateExpression = true;
-            state = "operator";
-          }
-          break;
-        default:
-          break;
-      }
-
-      break;
+  const isOperand = !isNaN(parseInt(bVal));
+  if (isOperand) { //need to check if last item in expression is an operator
+    operand !== "0" && !isNaN(parseInt(operand[operand.length-1])) ? 
+      operand += bVal : operand = bVal;
+    return [1, 0, 0];
   }
-  if (updateExpression) { expressionDisplay.innerHTML = expression.join(' ')}
-  //if (expressionDisplay.innerHTML === "") {expressionDisplay.innerHTML = "0"}
+  else {
+    switch(bVal) {
+      case "C" : return Clr();
+      case "DEL" : return DEL();
+      case "=" : 
+        expression.push(operand);
+        operand = evaluate(expression);
+        expression.push(bVal,operand);
+        expressions.push(expression);
+        return [1, 1, 1];
+      default:
+        operator = bVal;
+        //can check for new length with this, too
+        expression.push(operand,operator);
+        operand = evaluate(expression);
+        
+        return [1,1,0];
+    }
+  }
+
 };
 
-//when "=" is clicked
-//should return the result
-const evaluate = (e) => {
-  console.log(expression);
-};
-
-const C = () => {
-  operand = display.innerHTML = "0";
+const Clr = () => {
+  operand = "0";
   expression = [];
-  state = "clear";
+  return [1, 1, 0];
 };
 
+
+//*** Need checks for deleting from evaluated results */
 const DEL =  () => {
+  operand = operand.toString().slice(0,-1);
   //let's not leave the display empty
-  if (display.innerHTML.length === 1) { 
-    display.innerHTML = "0";
-    state = "clear";
-    return;
+  if (operand.length === 0) { 
+    operand = "0";
   }
-  display.innerHTML = display.innerHTML.slice(0,-1);
+  return [1, 0, 0];
 };
 
-const divide = () => {
+const getExprSplit_i = (e) => {
+  let topPriority_i = 0;
+  let topPriority = -1;
+  for (let i = 0; i < e.length; i += 2) { //loop through expression, skipping operators
+    if (ops.includes(e[i + 1])) { //check if the next value in the expression is an actionable operator
+      const priority = ops.indexOf(e[i + 1]); //set the current priority to the operator's place in the operations array (listed by descending PEMDAS)
+      if (priority > topPriority) { //check if that priority is higher than the currently recorded highest
+        topPriority = priority; //if it is, set the new top priority
+        topPriority_i = i + 1; //and record the index of that operator in the expression
+      }
+    }
+  }
+  return topPriority_i;
+} 
 
-};
+//**Need checks for evaluating expressions that end with an operator */
 
-const times = () => {
+const evaluate = (expr) => {
+  
+  //MAY NEED TO PERFORM ADDITIONAL EXPRESSION FORMAT CHECKS HERE
 
-};
+  if (expr.length < 3) { return parseFloat(expr[0]) } //if it's just a single operand, we can return that
 
-const minus = () => {
+  //this part is a little confusing
+  //counterintuitively, the expression needs to be split by operators in order of lowest PEMDAS
+    /* ex: 
+        eval(["1", "+", "2", "*", "3", "+", "4", "*", "5"]) 
+        becomes eval( eval["1", "+", "2", "*", "3"], "+", eval(["4", "*", "5"]) ) 
+        and so on..
+    */
+  //By splitting by the lower order PEMDAS operators,
+  //the higher order PEMDAS operations can fully evaluate before returning
 
-};
 
-const plus = () => {
+  //**NEED TO CHECK FOR CASES WHERE NOTHING IS ON RIGHT SIDE OF SELECTED OPERATOR */
+  const opIndex = getExprSplit_i(expr);//this function handles getting the index of the operator to split around
+  const op = expr[opIndex]; //this is that operator, needed for the outermost evaluation
+  const leftExpr = expr.slice(0, opIndex); //slice the array to yield the expression to the left of the priority operator
+  const rightExpr = expr.slice(opIndex + 1, opIndex.length); //and then the right as well
 
+  const leftVal = parseFloat(eval(leftExpr));
+  const rightVal = parseFloat(eval(rightExpr));
+
+  let result;
+  switch (op) {
+    case '+':
+      result = leftVal + rightVal;
+      break;
+    case '-':
+      result = leftVal - rightVal;
+      break;
+    case '×':
+      result = leftVal * rightVal;
+      break;
+    case '÷':
+      result = Math.floor(leftVal / rightVal);
+      break;
+    default:
+      throw new Error(`Invalid operator: ${op}`);
+  }
+
+  return result;
 };
